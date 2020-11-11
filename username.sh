5012a814719e2469a302e28926209c181bc851fb
Ellie Kolvereid <elliekolvereid@Ellies-Air.home>
2020-11-11 10:35:47 -0800

    updates regex

--- username.sh
+++ username.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 # username.sh 
 # Ellie Kolvereid
-echo "Enter a five-digit ZIP code: "
-read ZIP
-while echo "$ZIP" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+echo "Enter a username: "
+read NAME
+while echo "$username" | egrep -v "^[a-z][0-9]{5}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid ZIP code - five digits only!"
 	echo "Enter a five-digit ZIP code: "
