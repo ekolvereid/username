09485eb6f40684c1b5921e47848d41d2bd77f059
Ellie Kolvereid <elliekolvereid@Ellies-Air.home>
2020-11-11 10:44:43 -0800

    update regex

--- username.sh
+++ username.sh
@@ -3,10 +3,10 @@
 # Ellie Kolvereid
 echo "Enter a username: "
 read NAME
-while echo "$username" | egrep -v "^[a-z][0-9]{5}$" > /dev/null 2>&1
+while echo "$username" | egrep -v "^[a-z][0-9]{3,12}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+	echo "You must enter a valid username - lower case letters, digits only!"
+	echo "Enter a username: "
+	read NAME
 done
 echo "Thank you"
