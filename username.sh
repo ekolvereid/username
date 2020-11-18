1692bde3a20f8d3a67860668fd2f7faadf4d8745
Ellie Kolvereid <elliekolvereid@Ellies-MacBook-Air.local>
2020-11-17 19:34:32 -0700

    adjusting errors

--- username.sh
+++ username.sh
@@ -3,10 +3,10 @@
 # Ellie Kolvereid
 echo "Enter a username: "
 read -r USERNAME
-while echo "$username" | grep -E -V "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
+while echo "$USERNAME" | grep -E -V "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username- the only characters that can be used are: lower case letters, digits, the underscore character. MUST START WITH: lowercase letter. 3-12 characters"
 	echo "Enter a username: "
-	read NAME
+	read USERNAME
 done
 echo "Thank you"
