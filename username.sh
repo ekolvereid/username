e648b894b29b1bfb48ff19034c3c5496bdef5ad2
Ellie Kolvereid <elliekolvereid@Ellies-MacBook-Air.local>
2020-11-17 19:07:12 -0700

    re-updated array

--- username.sh
+++ username.sh
@@ -3,7 +3,7 @@
 # Ellie Kolvereid
 echo "Enter a username: "
 read -r USERNAME
-while echo "$username" | grep -E -V "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
+while echo "$username" | grep -E -V "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username- the only characters that can be used are: lower case letters, digits, the underscore character. MUST START WITH: lowercase letter. 3-12 characters"
 	echo "Enter a username: "
