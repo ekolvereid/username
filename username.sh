77123065dc992d2467bd01102a3c9c5d4630729d
Ellie Kolvereid <elliekolvereid@Ellies-MacBook-Air.local>
2020-11-17 18:52:16 -0700

    Adds username.sh

--- username.sh
+++ username.sh
@@ -2,10 +2,10 @@
 # username.sh 
 # Ellie Kolvereid
 echo "Enter a username: "
-read NAME
-while echo "$username" | egrep -v "^[a-z][0-9]{3,12}$" > /dev/null 2>&1
+read -r USERNAME
+while echo "$username" | grep -E -V "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid username - lower case letters, digits only!"
+	echo "You must enter a valid username- the only characters that can be used are: lower case letters, digits, the underscore character. MUST START WITH: lowercase letter. 3-12 characters"
 	echo "Enter a username: "
 	read NAME
 done
