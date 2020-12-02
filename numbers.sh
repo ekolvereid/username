47e683261f6d8cf551d2befa4dd119902c53794d
Ellie Kolvereid <elliekolvereid@Ellies-MacBook-Air.local>
2020-12-01 18:38:49 -0700

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,19 @@
+#! bin/bash
+#numbers.sh
+#Ellie Kolvereid
+echo "PLease enter a positive number: "
+read -r NUMBERS
+
+N=1
+while ["$N -le "$NUMBERS]
+do
+	if $((N%2)) -eq 0
+	then:
+		echo "$N is even"
+	else:
+		echo "$N is odd"
+	fi
+
+	N=$ [$N+1]
+done
+
